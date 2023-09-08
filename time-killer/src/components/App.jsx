b081a3fc6e939a083be2fb70dd36e284d44a0b5c
Dan McGrath <mcgrathd3254@yahoo.com>
2023-09-08 17:49:16 -0400

    Add effect to track window size

---
+++ time-killer/src/components/App.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import Header from "./header/Header";
+
+const App = () => {
+  const [windowDimension, setWindowDimension] = useState(null);
+
+  useEffect(() => {
+    setWindowDimension(window.innerWidth);
+  }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimension(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const isMobile = windowDimension <= 640;
+
+  return (
+    <>
+      <Header isMobile={isMobile} />
+    </>
+  );
+};
+
+export default App;

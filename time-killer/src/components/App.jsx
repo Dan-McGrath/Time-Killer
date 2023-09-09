579c03cbab96ddbd5468f4b803cc749c2d63e5ba
Dan McGrath <mcgrathd3254@yahoo.com>
2023-09-08 21:55:05 -0400

    Add hero and contact to App

--- time-killer/src/components/App.jsx
+++ time-killer/src/components/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./header/Header";
+import Hero from "./home/hero";
+import Contact from "./home/Contact";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Hero />
+      <Contact />
     </>
   );
 };

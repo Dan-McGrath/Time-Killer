636b212847d239bce87b5f88defc61e06662a5b9
Dan McGrath <mcgrathd3254@yahoo.com>
2023-09-08 21:54:39 -0400

    create hero section for homepage

---
+++ time-killer/src/components/home/hero.jsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="title">
+        <h1>Welcome to Time Killer!</h1>
+        <img src="" alt="" />
+        <p>
+          Select a game from the navigation above and kill some time playing
+          these quick and easy games!
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;

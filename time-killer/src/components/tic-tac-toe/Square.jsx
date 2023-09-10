d2dcaa13655c4912da543fa157bc060f7d689da6
Dan McGrath <mcgrathd3254@yahoo.com>
2023-09-09 21:57:06 -0400

    Add proptypes to square component

--- time-killer/src/components/tic-tac-toe/Square.jsx
+++ time-killer/src/components/tic-tac-toe/Square.jsx
@@ -1,7 +1,13 @@
+import PropTypes from "prop-types";
+
 const Square = ({ index, clickHandler }) => {
   return (
     <div className="square" onClick={clickHandler} data-index={index}></div>
   );
 };
 
+Square.propTypes = {
+  index: PropTypes.number,
+  clickHandler: PropTypes.func,
+};
 export default Square;

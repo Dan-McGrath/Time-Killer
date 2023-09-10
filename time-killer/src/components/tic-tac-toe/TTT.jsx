26d069673c452833e29ce42b96382ab3014ba9a5
Dan McGrath <mcgrathd3254@yahoo.com>
2023-09-09 21:53:35 -0400

    add gameboard to parent component

--- time-killer/src/components/tic-tac-toe/TTT.jsx
+++ time-killer/src/components/tic-tac-toe/TTT.jsx
@@ -1,10 +1,14 @@
 import Header from "../header/Header";
+import TttGameboard from "./TttGameboard";
+import "../../styles/ttt.scss";
+import "../../styles/reset.scss";
 
 const Ttt = () => {
   return (
     <>
       <Header />
       <h1>Tic Tac Toe</h1>
+      <TttGameboard />
     </>
   );
 };

e565d82127c3839a9cfe6be90a7bf6aeb4857caa
Dan McGrath <mcgrathd3254@yahoo.com>
2023-09-13 22:06:58 -0400

    seperate form elements

--- time-killer/src/components/home/Contact.jsx
+++ time-killer/src/components/home/Contact.jsx
@@ -2,24 +2,31 @@ const Contact = () => {
   return (
     <section className="contact">
       <form action="">
-        <label htmlFor="name">Name: </label>
-        <input type="" id="name" name="name" placeholder="John Doe" />
-        <label htmlFor="email">Email: </label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="email@email.com"
-        />
-        <label htmlFor="message">Message: </label>
-        <textarea
-          name="message"
-          id="message"
-          cols="70"
-          rows="1"
-          placeholder="Please leave feedback or any other games you would like to see!"
-        ></textarea>
-        <button></button>
+        <div className="form-name">
+          <label htmlFor="name">Name: </label>
+          <input type="" id="name" name="name" placeholder="John Doe" />
+        </div>
+        <div className="form-email">
+          <label htmlFor="email">Email: </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="email@email.com"
+          />
+        </div>
+        <div className="form-message">
+          <label htmlFor="message">Message: </label>
+          <textarea
+            name="message"
+            id="message"
+            cols="70"
+            rows="1"
+            placeholder="Please leave feedback or any other games you would like to see!"
+          ></textarea>
+        </div>
+
+        <button className="btn-submit">Submit</button>
       </form>
     </section>
   );

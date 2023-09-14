6f73c80509f03afbc59b9dba3e587111ad592e2b
Dan McGrath <mcgrathd3254@yahoo.com>
2023-09-13 20:48:16 -0400

    Add links to games in hero section

--- time-killer/src/components/home/hero.jsx
+++ time-killer/src/components/home/hero.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 const Hero = () => {
   return (
     <section className="hero">
@@ -5,9 +6,23 @@ const Hero = () => {
         <h1>Welcome to Time Killer!</h1>
         <img src="" alt="" />
         <p>
-          Select a game from the navigation above and kill some time playing
-          these quick and easy games!
+          Select a game from the navigation above or select one of the options
+          below, and kill some time playing these quick and easy games!
         </p>
+      </div>
+      <div className="game-selections">
+        <div className="blackjack">
+          <Link to="/Blackjack">Blackjack</Link>
+        </div>
+        <div className="memory">
+          <Link to="/Memory">Memory</Link>
+        </div>
+        <div className="tic-tac-toe">
+          <Link to="/Tic-Tac-Toe">Tic Tac Toe</Link>
+        </div>
+        <div className="battleship">
+          <Link to="/Battleship">Battleship</Link>
+        </div>
       </div>
     </section>
   );

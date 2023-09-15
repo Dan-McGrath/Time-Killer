4831c8cd637966bb9c40cca28e72f1cef5c3b56d
Dan McGrath <mcgrathd3254@yahoo.com>
2023-09-14 20:22:21 -0400

    Add PropTypes and create array of cards

---
+++ time-killer/src/components/memoryGame/MemoryGameboard.jsx
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+import Card from "./memoryCard";
+import Button from "../Button";
+const Gameboard = ({ pokemonArray, newArrayHandler }) => {
+  let cardArray = pokemonArray.map((ele) => <Card key={ele} index={ele} />);
+  return (
+    <>
+      <div className="memory-gameboard">{cardArray}</div>
+      <Button text="Get Cards" clickHandler={newArrayHandler} />
+    </>
+  );
+};
+Gameboard.propTypes = {
+  pokemonArray: PropTypes.array,
+  newArrayHandler: PropTypes.func,
+};
+
+export default Gameboard;

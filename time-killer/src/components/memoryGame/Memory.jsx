809c8ceedb9cfb8bbbbd599a6ebe61230a436287
Dan McGrath <mcgrathd3254@yahoo.com>
2023-09-14 20:22:39 -0400

    Add gamemanager to Memory component

--- time-killer/src/components/memoryGame/Memory.jsx
+++ time-killer/src/components/memoryGame/Memory.jsx
@@ -1,10 +1,13 @@
 import Header from "../header/Header";
+import GameManager from "./MemoryGameManager";
 
 const Memory = () => {
   return (
     <>
       <Header />
       <h1>Memory</h1>
+
+      <GameManager />
     </>
   );
 };

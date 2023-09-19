85737969dbd491cdee40a3fe55a8b29deee1ccf3
Dan McGrath <mcgrathd3254@yahoo.com>
2023-09-19 15:42:50 -0400

    change gameboard to display hands

--- src/components/blackjack/Gameboard.jsx
+++ src/components/blackjack/Gameboard.jsx
@@ -1,8 +1,16 @@
 import PropTypes from "prop-types";
 import Card from "./Card";
 
-const Gameboard = ({ currentDeck, faceUp }) => {
-  let deck = currentDeck.map((ele) => (
+const Gameboard = ({ currentPlayerCards, currentDealerCards, faceUp }) => {
+  let playerHand = currentPlayerCards.map((ele) => (
+    <Card
+      key={`${ele.getName()} of ${ele.getSuit()}`}
+      name={ele.getName()}
+      faceUp={faceUp}
+    />
+  ));
+
+  let dealerHand = currentDealerCards.map((ele) => (
     <Card
       key={`${ele.getName()} of ${ele.getSuit()}`}
       name={ele.getName()}
@@ -10,11 +18,17 @@ const Gameboard = ({ currentDeck, faceUp }) => {
       faceUp={faceUp}
     />
   ));
-  return <div className="gameboard">{deck}</div>;
+  return (
+    <>
+      <div className="player-hand">{playerHand}</div>
+      <div className="dealer-hand">{dealerHand}</div>
+    </>
+  );
 };
 
 Gameboard.propTypes = {
-  currentDeck: PropTypes.array,
+  currentPlayerCards: PropTypes.array,
+  currentDealerCards: PropTypes.array,
   faceUp: PropTypes.bool,
 };
 

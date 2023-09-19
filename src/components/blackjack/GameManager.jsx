3134de3cb6d11fe4a3f4815324b5ec04b4fccc8d
Dan McGrath <mcgrathd3254@yahoo.com>
2023-09-19 15:53:21 -0400

    create basic function to deal cards

--- src/components/blackjack/GameManager.jsx
+++ src/components/blackjack/GameManager.jsx
@@ -1,10 +1,14 @@
 import deck from "./bjLogic/createDeck";
+import player from "./bjLogic/player";
 import Gameboard from "./Gameboard";
 import Button from "../Button";
 import { useState } from "react";
 
 const GameManager = () => {
   const [faceUp, setFaceUp] = useState(true);
+  const [player1, setPlayer1] = useState(player("Player 1"));
+  const [dealer, setDealer] = useState(player("Dealer"));
+  const [currentPlayer, setCurrentPlayer] = useState(player1);
 
   const shuffleCards = (arr) => {
     const newArray = arr.slice(0);
@@ -18,11 +22,30 @@ const GameManager = () => {
 
   const [currentDeck, setCurrentDeck] = useState(newDeck);
 
-  const getDeck = () => setCurrentDeck(newDeck);
+  const getNewDeck = () => setCurrentDeck(newDeck);
+  if (currentDeck.length < 1) {
+    getNewDeck();
+  }
+
+  const dealCards = () => {
+    let currentCard = currentDeck.pop();
+    setPlayer1({ ...player1 }, player1.addCardToHand(currentCard));
+    currentCard = currentDeck.pop();
+    setDealer({ ...dealer }, dealer.addCardToHand(currentCard));
+    currentCard = currentDeck.pop();
+    setPlayer1({ ...player1 }, player1.addCardToHand(currentCard));
+    currentCard = currentDeck.pop();
+    setDealer({ ...dealer }, dealer.addCardToHand(currentCard));
+  };
+
   return (
     <>
-      <Gameboard currentDeck={currentDeck} faceUp={faceUp} />
-      <Button text="get Deck" clickHandler={getDeck} />
+      <Gameboard
+        currentPlayerCards={player1.getHand()}
+        currentDealerCards={dealer.getHand()}
+        faceUp={faceUp}
+      />
+      <Button text="Deal" clickHandler={dealCards} />
     </>
   );
 };

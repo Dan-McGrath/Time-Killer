3fbc2e53615cc6ddefa38ffe41232d25eda5e541
Dan McGrath <mcgrathd3254@yahoo.com>
2023-09-18 22:56:29 -0400

    Create main Blackjack component

--- src/components/blackjack/Blackjack.jsx
+++ src/components/blackjack/Blackjack.jsx
@@ -1,10 +1,12 @@
 import Header from "../header/Header";
+import GameManager from "./GameManager";
 
 const Blackjack = () => {
   return (
     <>
       <Header />
       <h1>BlackJack</h1>
+      <GameManager />
     </>
   );
 };

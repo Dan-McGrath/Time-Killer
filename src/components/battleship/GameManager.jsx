f3273d2b6571d6c63364e5c201c9f9df446404f6
Dan McGrath <mcgrathd3254@yahoo.com>
2023-09-23 17:52:15 -0400

    Create drag n drop functions

--- src/components/battleship/GameManager.jsx
+++ src/components/battleship/GameManager.jsx
@@ -12,10 +12,48 @@ const GameManager = () => {
 
   const currentPlayer = useRef(player1);
 
-  return (
-    <div className="battleship">
-      <Gameboard currentPlayer={currentPlayer.current} />
+  // drag and drop functions
+
+  let dragged;
+
+  const dragstartHandler = (e) => {
+    e.dataTransfer.dropEffect = "copy";
+    dragged = e.target;
+  };
+
+  const dragOverHandler = (e) => {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = "move";
+  };
+
+  const dropHandler = (e) => {
+    e.target.appendChild(dragged);
+  };
+
+  const currentPlayersShips = currentPlayer.current.ships.map((ele) => (
+    <div
+      className="ship"
+      key={ele.ship.getName()}
+      draggable="true"
+      onDragStart={dragstartHandler}
+      id={ele.ship.getName()}
+    >
+      <p>{ele.ship.getName()}</p>
+      <p>Size: {ele.ship.getLength()}</p>
     </div>
+  ));
+
+  return (
+    <>
+      <div className="battleship-gameboard">
+        <Gameboard
+          currentPlayer={currentPlayer.current}
+          dragOverHandler={dragOverHandler}
+          dropHandler={dropHandler}
+        />
+      </div>
+      <div className="ships">{currentPlayersShips}</div>
+    </>
   );
 };
 

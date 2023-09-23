b5dd9b2aeb920536a1afe2c45ef89ced5d109ad7
Dan McGrath <mcgrathd3254@yahoo.com>
2023-09-23 17:24:10 -0400

    add proptypes and drag n drop handlers

--- src/components/battleship/Gameboard.jsx
+++ src/components/battleship/Gameboard.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import Square from "./Square";
-const Gameboard = ({ currentPlayer }) => {
+const Gameboard = ({ currentPlayer, dragOverHandler, dropHandler }) => {
   let currentPlayerGameboard = currentPlayer.currentGameboard.map((ele) => (
     <Square
       key={ele.coordinate}
@@ -8,14 +8,18 @@ const Gameboard = ({ currentPlayer }) => {
       isAttacked={ele.isAttacked}
       isOccupied={ele.isOccupied}
       isMissed={ele.isMissed}
+      dragOverHandler={dragOverHandler}
+      dropHandler={dropHandler}
     />
   ));
 
   return <>{currentPlayerGameboard}</>;
 };
 
-Gameboard.proptypes = {
+Gameboard.propTypes = {
   currentPlayer: PropTypes.object,
+  dragOverHandler: PropTypes.func,
+  dropHandler: PropTypes.func,
 };
 
 export default Gameboard;

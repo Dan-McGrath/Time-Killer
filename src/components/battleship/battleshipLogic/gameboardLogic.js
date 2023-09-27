ad4b69aa58b862463e1fc3143286b6984619e622
Dan McGrath <mcgrathd3254@yahoo.com>
2023-09-27 19:26:28 -0400

    add index for squares

--- src/components/battleship/battleshipLogic/gameboardLogic.js
+++ src/components/battleship/battleshipLogic/gameboardLogic.js
@@ -2,14 +2,17 @@ const gameboard = () => {
   const gameboardArr = [];
 
   const createGameboard = () => {
+    let index = 0;
     for (let i = 0; i <= 9; i++) {
       for (let j = 0; j <= 9; j++) {
         const square = {
+          index: index,
           coordinate: [i, j],
           isOccupied: false,
           isAttacked: false,
           isMissed: false,
         };
+        index += 1;
         gameboardArr.push(square);
       }
     }

7bb6369a04258fdcd0eed1e7b8a43daa616dcdc5
Dan McGrath <mcgrathd3254@yahoo.com>
2023-09-30 17:43:10 -0400

    remove unneeded arr

--- src/components/battleship/GameManager.jsx
+++ src/components/battleship/GameManager.jsx
@@ -16,7 +16,6 @@ const GameManager = () => {
   const [isHorizontal, setIsHorizontal] = useState(false);
   const currentPlayer = useRef(player1);
 
-  const horizontalCollisons = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
   // drag and drop functions
 
   let dragged;
